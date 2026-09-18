4eb942d9f49233996081be2f9d1774d31669c0d8
SSOMEYA-CODE <s-someya@weing.co.jp>
2026-09-18 13:48:39 +0900

    Correct JSON method and update Streamlit display
    Fix typo in JSON method and update display function.

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ if ingredients_list:
 import requests
 # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+sf_df = st.text(smoothiefroot_response.jason(), use_container_width=True)
