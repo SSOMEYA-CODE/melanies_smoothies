d920d1c969597adddd4f53142221c4c72c2bd47d
SSOMEYA-CODE <s-someya@weing.co.jp>
2026-09-18 16:54:55 +0900

    Refactor ingredient processing in streamlit_app.py
    Removed duplicate ingredient processing logic and adjusted the structure for clarity.

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,19 @@ ingredients_list = st.multiselect(
 	)
 
 if ingredients_list:
+    ingredients_string = ' '
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ''
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+		
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+if ingredients_list:
 
     ingredients_string = ''
 
@@ -52,16 +65,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
-
-if ingredients_list:
-    ingredients_string = ' '
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-		
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
